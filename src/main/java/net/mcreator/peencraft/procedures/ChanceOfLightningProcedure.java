f8a9ab393ee44b64c47f21cd40087af46c80121d
MrCenji <Carter@DESKTOP-JRNRB3B>
2022-05-20 22:11:54 -0400

    Every block broken is a %1 chance of lightning

---
+++ src/main/java/net/mcreator/peencraft/procedures/ChanceOfLightningProcedure.java
@@ -0,0 +1,38 @@
+package net.mcreator.peencraft.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.world.BlockEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.BlockPos;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class ChanceOfLightningProcedure {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent event) {
+		execute(event, event.getWorld(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z) {
+		execute(null, world, x, y, z);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z) {
+		if (Math.random() < 0.01) {
+			if (world instanceof ServerLevel _level) {
+				LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
+				entityToSpawn.moveTo(Vec3.atBottomCenterOf(new BlockPos(x, y, z)));
+				entityToSpawn.setVisualOnly(false);
+				_level.addFreshEntity(entityToSpawn);
+			}
+		}
+	}
+}
